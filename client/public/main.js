5eae93166e78586c731898ef963b2ae7c26574bf
tarek tito <tarekt820@gmail.com>
2020-05-03 01:12:41 +0200

    update path

--- client/public/main.js
+++ client/public/main.js
@@ -1,6 +1,7 @@
 const {execFile} = require('child_process');
 const open = require('open')
 const config = require('../config');
+const path =require('path')
 const chooseFileBtn = document.querySelector('.choose-file');
 
 
@@ -8,7 +9,7 @@ chooseFileBtn.addEventListener('click', callJupyterCell);
 
 async function callJupyterCell(e) {
    try {
-    await open(__dirname + '\\' + 'webcam.pyc',{wait:true})
+    await open(path.join(__dirname ,'..' ,'..', 'backend\\', 'webcam.pyc'),{wait:true})
    }catch(e) {
        console.error(e)
    }
