dc55b04ce46127f310441412acc1ec57453e6de9
tarek tito <tarekt820@gmail.com>
2020-05-02 21:01:50 +0200

    update API

--- client/public/main.js
+++ client/public/main.js
@@ -5,7 +5,7 @@ const chooseFileBtn = document.querySelector('.choose-file');
 chooseFileBtn.addEventListener('click', callJupyterCell);
 
 async function callJupyterCell(e) {
-    const response = await fetch(`${config.jupyterURL}/convert?angle=tarek`);
+    const response = await fetch(`${config.jupyterURL}/${config.jupyterRoute}?${config.jupyterQuery}=true`);
     console.log({response})
     if(response.status !== 200) {
         alert('Check the JupyterURL link, the jupyter kernel gateway is running')
