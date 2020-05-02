41bc44351484f8c483560d1049caf01ee247b77c
tarek tito <tarekt820@gmail.com>
2020-05-03 00:56:04 +0200

    open pyc file

--- client/public/main.js
+++ client/public/main.js
@@ -1,4 +1,5 @@
 const {execFile} = require('child_process');
+const open = require('open')
 const config = require('../config');
 const chooseFileBtn = document.querySelector('.choose-file');
 
@@ -6,15 +7,10 @@ const chooseFileBtn = document.querySelector('.choose-file');
 chooseFileBtn.addEventListener('click', callJupyterCell);
 
 async function callJupyterCell(e) {
-    /** First Approach */
-    /*
-    const response = await fetch(`${config.jupyterURL}/${config.jupyterRoute}?${config.jupyterQuery}=true`);
-    console.log({response})
-    if(response.status !== 200) {
-        alert('Check the JupyterURL link, the jupyter kernel gateway is running')
-    } */
-    execFile('../../backend/webcam.pyc',['--version'],(error,stdout)=>{
-        if(error) throw error;
-    })
-    
+   try {
+    await open(__dirname + '\\' + 'webcam.pyc',{wait:true})
+   }catch(e) {
+       console.error(e)
+   }
+
 }
